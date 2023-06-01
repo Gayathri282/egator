94f4b16dd616fd367f508e7f395c6056ea4eb7c1
Gayathri282 <109743544+Gayathri282@users.noreply.github.com>
2023-06-01 10:53:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+// changing navbar style on scrolling
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll' ,window.scrollY>0)
+})
+
+//faqs show/hide
+const faqs=document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon=faq.querySelector('.faq_icon i');
+        if(icon.className==='uil uil-plus'){
+            icon.className="uil uil-minus";
+        }else{
+            icon.className="uil uil-plus";
+        }
+
+    })
+})
